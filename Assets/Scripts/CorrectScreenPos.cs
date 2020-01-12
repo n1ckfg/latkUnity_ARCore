3016eed64f8922fea20df5e8b61688c36edafc69
Nick Fox-Gieg <nick@fox-gieg.com>
2020-01-12 18:06:27 -0500

    remove speech submodule

---
+++ Assets/Scripts/CorrectScreenPos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectScreenPos : MonoBehaviour {
+
+    public Vector2 margin = Vector2.zero;
+
+    private bool firstRun = true;
+
+    void Update() {
+        if (firstRun) {
+            Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(margin.x - (Screen.width / 2), (Screen.height + (Screen.height / 2)) - margin.y, 1f));
+            transform.localPosition = new Vector3(newPos.x, newPos.y, transform.localPosition.z);
+
+            firstRun = false;
+        }
+    }
+
+}
